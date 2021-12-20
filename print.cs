172176ca4683e5670f1b38c75d80871732efc95c
windows777-dev <75386815+windows777-dev@users.noreply.github.com>
2021-12-20 18:25:29 +0000

    Create print.cs
    This is a file that prints out something to the console!

---
+++ print.cs
@@ -0,0 +1,22 @@
+// This is a program that prints out something!
+
+using System;
+
+namespace <Program name here>
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string forwardd = Console.ReadLine();
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            
+        }
+    }
+}
+
+// 
+// Please note that you need the .NET for this to work plus in the terminal you need to write dotnet new console.
+// In order to find a video that helps please go to: https://www.youtube.com/watch?v=jGD0vn-QIkg&t=2s to find out.
