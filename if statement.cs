a6a3913c163444df5d839a66c40d0774f5750bea
windows777-dev <75386815+windows777-dev@users.noreply.github.com>
2021-12-20 19:58:17 +0000

    Create if statement.cs

---
+++ if statement.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace <Program Name Here>
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int a = 10;
+            string forwardd = Console.ReadLine();
+            if (a == 10);
+            {
+            // Your code here but I will write my own
+            Console.WriteLine(a*a)
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
