ad0c2db29b0c290f0a3c4e4a311548b401a98da4
windows777-dev <75386815+windows777-dev@users.noreply.github.com>
2021-12-20 20:04:49 +0000

    Create While_Loop.cs

---
+++ While_Loop.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace <Folder name here>
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write your own code smh >:(
+            int a = 5;
+            while (1+1==2)
+            {
+                Console.WriteLine(a);
+            }
+            
+        }
+    }
+}
