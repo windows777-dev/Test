140948bc33b3552d0a6641938e3296bf0e9f0513
windows777-dev <75386815+windows777-dev@users.noreply.github.com>
2021-12-20 20:07:55 +0000

    Create for_loop.cs

---
+++ for_loop.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace LOL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int a = 5;
+        for (a == 5)
+        {
+        // Your code here
+        }
+            
+        }
+    }
+}
